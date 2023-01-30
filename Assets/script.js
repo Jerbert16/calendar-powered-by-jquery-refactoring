d79a7523945cd9c484653fdf47cf44afa5c3ab98
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-29 20:19:12 -0700

    completed local storage. Now functions properly.

--- Assets/script.js
+++ Assets/script.js
@@ -31,40 +31,41 @@
 
 $(function () {
   //Set date to display current day of week and MM/DD/YYYY
+  const timeClrEl = $(".time-block");
+  const calTimeEl = $("div > div > div").text()
+ 
+
   const currentDay = dayjs().format("dddd, MMM D, YYYY");
-  console.log(currentDay);
+  const currentHr = 12 //dayjs().format("H");
+
+  console.log(calTimeEl);
+
   $("#currentDay").text(currentDay);
 
-  // localStorage.setItem('events', JSON.stringify());
-
-  
-  $("button").on('click', function () {
+  $("button").on("click", function () {
     let timeOfDay = $(this).parent().attr("id");
     let inputText = $(this).siblings(".description").val();
     localStorage.setItem(timeOfDay, inputText);
    });
 
 
- 
-   
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+   if (currentHr < calTimeEl){ 
+   timeClrEl.attr('class', 'row time-block past');
+   }
+   if (currentHr == calTimeEl) {
+    timeClrEl.attr('class', 'row time-block present');
+   } 
+   if (currentHr > calTimeEl) {
+    timeClrEl.attr('class', 'row time-block future');
+   }
+
+   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
